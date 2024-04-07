67181605907586603be2a1bc535707694da68635
patrickangeli <patrick.angeli@ufv.br>
2024-04-06 23:28:27 -0300

    update code

--- scripts/main_multi_perceptron_classificacao.py
+++ scripts/main_multi_perceptron_classificacao.py
@@ -179,9 +179,9 @@ def create_sequences(data_x, data_y, tempo_antecedencia, lst_datas, num_steps):
 if __name__ == "__main__":
     main(file_input_cota='../Entrada/cota.csv',
          file_input_chuva='../Entrada/chuva.csv',
-         tempo_antecedencia=24, #14,16,20,24
+         tempo_antecedencia=8, #14,16,20,24
          num_steps=6,
-         num_neurons=60,        #24,36,48,72
+         num_neurons=72,        #24,36,48,72
          num_epochs=500,
          func_camada_oculta= 'relu',
          func_camada_saida= 'sigmoid',
